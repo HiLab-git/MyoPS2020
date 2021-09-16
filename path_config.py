eeba01c02d63c233aa6b2271c7f633dae8d8b76c
Guotai Wang <wguotai@gmail.com>
2021-09-16 10:07:54 +0800

    update requirements and add evaluation configure

--- path_config.py
+++ path_config.py
@@ -1,4 +1,4 @@
-import os 
+# -*- coding: utf-8 -*-
 path_dict = {}
 path_dict['MyoPS_data_dir'] = "/home/guotai/disk2t/data/heart/MyoPS"
 path_dict['nnunet_raw_data_dir'] = "/home/guotai/disk2t/projects/nnUNet/exp_dir/nnUNet_raw/nnUNet_raw_data"
